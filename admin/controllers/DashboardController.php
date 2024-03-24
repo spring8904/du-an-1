261ac7277cde6b6229154de3c72b8c16407fa2da
spring8904 <lamthang890123music@gmail.com>
2024-03-24 12:56:23 +0700

    hoan thien chuc nang ql user

--- admin/controllers/DashboardController.php
+++ admin/controllers/DashboardController.php
@@ -3,5 +3,6 @@
 function dashboard()
 {
   $view = 'dashboard';
+  $scripts = ['scripts/dashboard'];
   require_once PATH_VIEW_ADMIN . 'layouts/master.php';
 }
---
+++ admin/views/users/detail.php
@@ -0,0 +1,70 @@
+<div class="container-fluid">
+  <!-- Page Heading -->
+  <h1 class="h3 mb-2 text-gray-800"><?= $title ?></h1>
+
+  <!-- DataTales Example -->
+  <div class="card shadow mb-4">
+    <div class="card-header py-3">
+      <h6 class="m-0 font-weight-bold text-primary d-flex justify-content-between">
+        <a href="<?= BASE_URL_ADMIN ?>?act=users">Quay trở lại</a>
+        <a href="<?= BASE_URL_ADMIN ?>?act=user-update&id=<?= $user['id'] ?>">Sửa thông tin</a>
+      </h6>
+    </div>
+    <div class="card-body">
+      <div class="table-responsive">
+        <table class="table table-bordered" id="" width="100%" cellspacing="0">
+          <thead>
+            <tr>
+              <th>Trường dữ liệu</th>
+              <th>Dữ liệu</th>
+            </tr>
+          </thead>
+          <tfoot>
+            <tr>
+              <th>Trường dữ liệu</th>
+              <th>Dữ liệu</th>
+            </tr>
+          </tfoot>
+          <tbody>
+            <tr>
+              <td>ID</td>
+              <td><?= $user['id'] ?></td>
+            </tr>
+            <tr>
+              <td>Ảnh đại diện</td>
+              <td><img src="<?= BASE_URL ?>uploads/<?= $user['avatar'] ?>" alt="avatar" width="100"></td>
+            </tr>
+            <tr>
+              <td>Email</td>
+              <td><?= $user['email'] ?></td>
+            </tr>
+            <tr>
+              <td>Mật khẩu</td>
+              <td>********</td>
+            </tr>
+            <tr>
+              <td>Họ tên</td>
+              <td><?= $user['ho_ten'] ?></td>
+            </tr>
+            <tr>
+              <td>Giới tính</td>
+              <td><?= $user['gioi_tinh'] == 'male' ? 'Nam' : 'Nữ' ?></td>
+            </tr>
+            <tr>
+              <td>Ngày sinh</td>
+              <td><?= $user['ngay_sinh'] ?></td>
+            </tr>
+            <tr>
+              <td>Địa chỉ</td>
+              <td><?= $user['dia_chi'] ?></td>
+            </tr>
+            <tr>
+              <td>Chức vụ</td>
+              <td><?= ucfirst(showOne('tb_chuc_vu', $user['id_cv'])['chuc_vu']) ?></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
