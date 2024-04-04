de809f98d7b1111973eb07c7e2ea7b782bb64b4f
spring8904 <lamthang890123music@gmail.com>
2024-04-04 10:46:20 +0700

    Hoàn thành trang liên hệ

--- controllers/ContactController.php
+++ controllers/ContactController.php
@@ -7,6 +7,7 @@ function showFormContact()
     }
 
     require_once PATH_VIEW . 'contact.php';
+    unset($_SESSION['error']);
     unset($_SESSION['success']);
 }
 
@@ -38,21 +39,25 @@ function contactSend()
 function validateContact()
 {
     $err = [];
-
-    if (empty($_POST['ten_kh'])) {
-        $err['ten_kh'] = 'Vui lòng nhập Họ tên';
-    }
-    if (empty($_POST['email'])) {
-        $err['email'] = 'Vui lòng nhập email';
-    }
-    if (empty($_POST['so_dien_thoai'])) {
-        $err['so_dien_thoai'] = 'Vui bạn số điện thoại';
-    }
     if (empty($_POST['tieu_de'])) {
         $err['tieu_de'] = 'Vui lòng nhập tiêu đề';
     }
+    if (empty($_POST['ten_kh'])) {
+        $err['ten_kh'] = 'Vui lòng nhập họ tên';
+    }
+    if (empty($_POST['email'])) {
+        $err['email'] = 'Vui lòng nhập email';
+    } elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        $err['email'] = 'Email không hợp lệ';
+    }
+    if (empty($_POST['so_dien_thoai'])) {
+        $err['so_dien_thoai'] = 'Vui bạn số điện thoại';
+    } elseif (!preg_match('/^[0-9]{10,11}$/', $_POST['so_dien_thoai'])) {
+        $err['so_dien_thoai'] = 'Số điện thoại không hợp lệ';
+    }
+
     if (empty($_POST['noi_dung'])) {
-        $err['noi_dung'] = 'Vui lòng nhập lời nhắn';
+        $err['noi_dung'] = 'Vui lòng nhập nội dung';
     }
 
     return $err;
